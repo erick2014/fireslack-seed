5ba1bd95227096202852fcc4315b741ad5d43aa4
erick2014 <erickarmandogarcia@hotmail.com>
2015-07-08 15:34:18 -0500

    Change some methods implementation into users.service.

--- public/users/users.service.js
+++ public/users/users.service.js
@@ -17,21 +17,10 @@ angular.module("angularfireSlackApp")
 			},
 			//get the display name using the uid of the current logged in user
 			getDisplayName:function(uid){
-				//first get the data from firebase
-				return users.$loaded()
-							//once data is ready, return the displayName
-							.then(function(data){
-								//get a specific user using its uid
-								return users.$getRecord(uid).displayName;
-							})
+				return users.$getRecord(uid).displayName;
 			},
 			getGravatar:function(uid){
-				//once data is ready, return a gravatar passin in an email hashed
-				return users.$loaded()
-							.then(function(){
-								return '//www.gravatar.com/avatar/'+users.$getRecord(uid).emailHash;
-							});
-				
+				return '//www.gravatar.com/avatar/'+users.$getRecord(uid).emailHash;
 			},
 			//return all records as array
 			all:users
