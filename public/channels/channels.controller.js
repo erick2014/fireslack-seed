f48fcfc28a8aef15d2e4bd289766bc741dc73bc3
erick2014 <erickarmandogarcia@hotmail.com>
2015-07-13 13:04:23 -0500

    Add createChannel method also newChannel model into channels.controller file.

--- public/channels/channels.controller.js
+++ public/channels/channels.controller.js
@@ -1,27 +1,38 @@
 /*
-	Injects channels promise(defined into profile state)
-	Injects profile dependency(defined at profile state)
-	Injects Users Service(defined at users module)
-	Injects Auth Service(defined at auth module)
-	Injects $state service from ui-router
+  Injects channels dependency(defined into channels state)
+  Injects profile dependency(defined at profile state)
+  Injects Users Service(defined at users module)
+  Injects Auth Service(defined at auth module)
+  Injects $state service from ui-router
 */
 angular.module('angularfireSlackApp')
-	.controller('channelCtrl',function(channels,profile,Users,Auth,$state){
-		var self=this;
-		//set the channels resolved from profile state
-		self.channels=channels;
-		//set profile resolved from profile state
-		self.profile=profile;
+  .controller('channelCtrl',function(channels,profile,Users,Auth,$state){
+    var self=this;
+    //set the channels resolved from profile state
+    self.channels=channels;
+    //set profile resolved from profile state
+    self.profile=profile;
+    self.newChannel={name:""};
 
-		//inherit some methods from Users service
-		self.getDisplayName=Users.getDisplayName;
-		self.getGravatar=Users.getGravatar;
+    //inherit some methods from Users service
+    self.getDisplayName=Users.getDisplayName;
+    self.getGravatar=Users.getGravatar;
 
-		self.logout=function(){
-			Auth.$unauth();
-			$state.go("home");
-		}
+    self.logout=function(){
+      Auth.$unauth();
+      $state.go("home");
+    }
 
-		console.log("initializing channels controller, profile=",profile);
+    self.createChannel=function(){
+      //add a channel to the firebaseArray
+      self.channels.$add(self.newChannel )
+      //once is added, clear out newChannel object
+      .then(function(ref){
+        console.log("channel with id",ref.key()," added")
+        self.newChannel.name=""
+      })
+    }
 
-	});
+    console.log("initializing channels controller, channels=",channels);
+
+  });
